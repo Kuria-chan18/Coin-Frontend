4d44bf09bdca67739547128edd2431e29e16e4ce
Dev-Tary <tn-junior@hotmail.com>
2024-05-14 15:46:50 -0300

    Update LoginPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Login from './loginComponents/Login'; // Importe o componente de login
-import './loginComponents/Login.css';
+import Login from './loginComponents/Login'; // Importe o componente de login com o caminho correto
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <Login /> {/* Chame o componente de login aqui */}
-      </header>
+      <div className="coin-title">COIN</div> {/* Adiciona a palavra "COIN" acima do container de login */}
+      <Login /> {/* Chame o componente de login aqui */}
     </div>
   );
 }
