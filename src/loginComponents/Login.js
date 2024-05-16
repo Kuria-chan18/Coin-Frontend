1116cbe825ffc813a4ce9a050c13b848db0ff3aa
tn-junior <juju2004.tary@gmail.com>
2024-05-16 14:51:10 -0300

    updating  loginPage

--- src/loginComponents/Login.js
+++ src/loginComponents/Login.js
@@ -14,19 +14,24 @@ function Login() {
 
   return (
     <div className="login-container">
-      <div className="login-box">
-        <form onSubmit={handleLogin}>
-          <div>
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div>
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <div className="forgot-password"> {/* Div para estilizar o link */}
-            <a href="/forgot-password" className="forgot-password-link">Esqueci a senha</a> {/* Link para a opção "Esqueci a senha" */}
-          </div>
-          <button type="submit">Entrar</button>
-        </form>
+      <div className="login-content">
+        <div className="login-box">
+          <form onSubmit={handleLogin}>
+            <div>
+              <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div>
+              <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div className="forgot-password"> {/* Div para estilizar o link */}
+              <a href="/forgot-password" className="forgot-password-link">Esqueci a senha</a> {/* Link para a opção "Esqueci a senha" */}
+            </div>
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+        <div className="signup-link">
+          Ainda não é colaborador? <a href="/cadastro">Cadastre-se agora</a>
+        </div>
       </div>
     </div>
   );
